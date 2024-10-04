f2b43f37c7bc5a1d0f7f021791d0b8b407aaaa8d
zeno <zenosaletti@gmail.com>
2024-10-04 14:00:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Next Generation Networks
+
+![cover-of-these-notes](./src/cover/graphics/ngnbookcover.jpg)
---
+++ src/cover/graphics/covupdate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COVER_NAME=ngnbookcover
+DOWNLOAD_PATH=~/Downloads
+PROJECT_PATH=~/uni/crs/ayiii/ngn
+
+mv $DOWNLOAD_PATH/$COVER_NAME* $PROJECT_PATH/src/cover/graphics
